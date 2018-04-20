8069d9b53de51c3fc75cf9ea1f5944614c5783a3
Steven Harrigan <def.monk0@gmail.com>
2018-04-20 19:18:02 -0400

    Added the http module to the project. Added our ESI library to the project.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 // Native
 import { BrowserModule } from "@angular/platform-browser";
+import { CommonModule } from "@angular/common";
 import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
 import { NgModule } from "@angular/core";
 
 // External
@@ -8,24 +10,40 @@ import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 import { NgxElectronModule } from "ngx-electron";
 
 // Generated
+import { ApiModule } from "./esi/api.module";
+import { Configuration } from "./esi/configuration";
 
 // Internal
 import { AppComponent } from "./app.component";
+import { ApplicationDetailsComponent } from "./application-details/application-details.component";
 import { EveSsoService } from "./eve-sso/eve-sso.service";
 import { ManageCharactersComponent } from "./manage-characters/manage-characters.component";
 import { OverviewComponent } from "./overview/overview.component";
 
 // Routing
 import { AppRoutingModule } from "./app-routing.module";
-import { ApplicationDetailsComponent } from './application-details/application-details.component';
+
+// ESI API Configuration
+export const apiConfig = new Configuration({});
+export function getApiConfig() {
+	return apiConfig;
+}
 
 @NgModule({
 	bootstrap: [AppComponent],
-	declarations: [AppComponent, ManageCharactersComponent, OverviewComponent, ApplicationDetailsComponent],
+	declarations: [
+		AppComponent,
+		ApplicationDetailsComponent,
+		ManageCharactersComponent,
+		OverviewComponent,
+	],
 	imports: [
+		ApiModule.forRoot(getApiConfig),
 		AppRoutingModule,
 		BrowserModule,
+		CommonModule,
 		FormsModule,
+		HttpClientModule,
 		NgbModule.forRoot(),
 		NgxElectronModule,
 	],
