7a23fc4a563e894f0b7dbccd9471e53b14da4b40
Steven Harrigan <def.monk0@gmail.com>
2018-04-23 00:00:29 -0400

    Fixed references to character-info sub-components. Fixed usage of routerLink.

--- src/app/character-info/character-info.component.ts
+++ src/app/character-info/character-info.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from "@angular/core";
 
+import { SkillQueueComponent } from "./skill-queue/skill-queue.component";
+import { SkillsComponent } from "./skills/skills.component";
+import { WalletComponent } from "./wallet/wallet.component";
+
 @Component({
 	selector: "app-character-info",
 	templateUrl: "./character-info.component.html",
